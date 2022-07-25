597750fb210c4d8e98c3b276532bb5cb86ba8c6f
Tony Stark <Tony Stark@DESKTOP-GL2TOUC>
2022-07-25 13:31:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string phrase = "GROOT";
+char grade = 'a';
+int x = 3;
+double y = 50.5;
+bool z = true;
+Console.WriteLine("The variables currently explored are :");
+Console.WriteLine("This is a string " + phrase + " ,This a char " + grade + " ,This an integer " + x + " This a decimal " + y);
+Console.WriteLine("And of course all I said is a boolean :"+ z);
+
